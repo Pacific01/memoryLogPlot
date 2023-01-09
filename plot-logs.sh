33d85eba12ad7313dc6a192daff3866f9439106e
Jaime Pascual Torres <jpascual@everlyrusher.com>
2023-01-09 18:49:31 +0100

    Save images as .png

--- plot-logs.sh
+++ plot-logs.sh
@@ -2,5 +2,5 @@
 
 for file in logs/*.log; do
   fileName=${file/logs/}
-  gnuplot -e "datafile='$file'; outputname='plots$fileName'" gnuplot.p
+  gnuplot -e "datafile='$file'; outputname='plots$fileName.png'" gnuplot.p
 done
