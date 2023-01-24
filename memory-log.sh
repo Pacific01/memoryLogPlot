4a6589a5bcb9a5a6531e971687555415cb545493
Jaime Pascual Torres <jpascual@everlyrusher.com>
2023-01-24 18:04:44 +0100

    Fix using argument instead of hardcoded device

--- memory-log.sh
+++ memory-log.sh
@@ -9,7 +9,7 @@ then
     do
         tempfs=$(du /mnt/ram | awk '{print $1}')
         memory=$(free --kilo | grep Mem: | awk '{print $3}')
-        kb_read_write=$(sar -d 1 1 | grep -m1 md0 | awk '{print $4 " " $5}')
+        kb_read_write=$(sar -d 1 1 | grep -m1 $1 | awk '{print $4 " " $5}')
         echo $memory $tempfs $kb_read_write >> ./logs/$logfile
         sleep 1
     done
